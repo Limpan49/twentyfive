337b39c37bff8f7ccc01fc375fdb595a7c5bd65a
Limpan49 <limparlimpan@gmail.com>
2025-03-23 22:31:11 +0100

    2025-03-23

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-/*document.getElementById("menu-toggle").addEventListener("click", function() {
-    var menu = document.querySelector(".menu");
-    var fadeLayer = document.querySelector(".fade-layer");
-    var icon = this.querySelector("i.material-icons");
-  
-    // Toggle the menu visibility
-    menu.classList.toggle("show");
-    fadeLayer.classList.toggle("visible");
-  
-    // Change the icon when the menu is open or closed
-    if (menu.classList.contains("show")) {
-      icon.textContent = "close"; // Change icon to "close"
-    } else {
-      icon.textContent = "menu"; // Change icon to "menu"
-    }
-  });
-*/
-
-
 document.getElementById("menu-toggle").addEventListener("click", function(event) {
     var menu = document.querySelector(".menu");
     var fadeLayer = document.querySelector(".fade-layer");
@@ -26,9 +7,9 @@ document.getElementById("menu-toggle").addEventListener("click", function(event)
     fadeLayer.classList.toggle("visible");
 
     if (menu.classList.contains("show")) {
-        icon.textContent = "close"; // Ändra till "close" när menyn är öppen
+        icon.textContent = "close";
     } else {
-        icon.textContent = "menu"; // Ändra till "hamburgemenu" när den är stängd
+        icon.textContent = "menu";
     }
 
     event.stopPropagation();
@@ -39,7 +20,7 @@ document.addEventListener("click", function(event) {
     var fadeLayer = document.querySelector(".fade-layer");
 
     if (!menu.contains(event.target) && !fadeLayer.contains(event.target)) {
-        // Stäng menyn och fade-lagret om klicket var utanför
+        // Stäng menyn och fade-lagret om klicket var utanför dock inte utanför det gråa lagret 
         menu.classList.remove("show");
         fadeLayer.classList.remove("visible");
 
