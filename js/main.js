8c3fba6f7b364721714f06f5546997e1167aac46
Limpan49 <limparlimpan@gmail.com>
2025-04-09 08:58:17 +0200

    2025-04-09

--- js/main.js
+++ js/main.js
@@ -19,8 +19,7 @@ document.addEventListener("click", function(event) {
     var menu = document.querySelector(".menu");
     var fadeLayer = document.querySelector(".fade-layer");
 
-    if (!menu.contains(event.target) && !fadeLayer.contains(event.target)) {
-        // Stäng menyn och fade-lagret om klicket var utanför dock inte utanför det gråa lagret 
+    if (!menu.contains(event.target)) {
         menu.classList.remove("show");
         fadeLayer.classList.remove("visible");
 
@@ -40,3 +39,19 @@ function myFunction(divId) {
     }
   }
   
+
+  // Skellettkod från w3 schools
+  let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("karusell");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000);
+}
